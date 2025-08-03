957ef9aed47ed96743b8e1153751ab8ef8bd9461
xxxijustwei <owner@ednesdayw.com>
2025-08-03 10:30:28 +0800

    init

---
+++ next.config.ts
@@ -0,0 +1,8 @@
+import withRspack from "next-rspack";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+export default withRspack(nextConfig);
